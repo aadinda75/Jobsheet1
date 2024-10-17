8b6e1741de8947db53188be29d485148115e1473
aadinda75 <dindaluluk209@gmail.com>
2024-10-17 19:12:36 +0700

    Jobsheet pertama

---
+++ Latihan03.java
@@ -0,0 +1,10 @@
+/**
+ * Latihan03
+ */
+public class Latihan03 {
+
+    public static void main(String[] args) {
+        System.out.println ("nama saya Adinda Luluk Hanifah");
+        System.out.println ("saya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+    }
+}
